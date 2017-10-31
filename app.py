0022b54d59114c83d6dbc78a1cb822549ec195ff
zlr <zlr@zlr.zlr>
2017-10-31 11:50:45 +0100

    using secrets

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
  
-import tweepy, time, sys
+import tweepy, time, sys, os
  
 tweets = 'tweets.txt'
 
- 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_KEY = ''
-ACCESS_SECRET = ''
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+ACCESS_KEY = os.environ["ACCESS_KEY"]
+ACCESS_SECRET = os.environ["ACCESS_SECRET"]
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
