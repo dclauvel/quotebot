ce41b21c2bd2364163af60fd4c8e421bd7828b65
zlr <zlr@zlr.zlr>
2017-10-31 08:18:03 +0100

    tweets from file

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import tweepy, time, sys
 tweets = 'tweets.txt'
 
  
-CONSUMER_KEY = '0AzowIioWSCd8Vi1gM4ghFn9p'
-CONSUMER_SECRET = 'yh7JYmZptDeWs2Ivg7SlSZ0QA3bvI9iLENtGM15PxfoApcKskd'
-ACCESS_KEY = '925124098627440640-Wx5xqEzfPMOLxG9XttcSOmsFSOohyvq'
-ACCESS_SECRET = 'tMsNz5CgeeW51IFqoGb0eNrWxSvilo6J3wt6XGyJCasEP'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
